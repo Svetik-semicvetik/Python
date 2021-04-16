e0feee0a4dce0ea3a91d4f932b909c6eb80eba52
Оськина Светлана Владимировна <oskina@ursalab.ru>
2021-04-16 20:36:51 +0500

    Исправленное задание 1 к уроку 9

--- Task-9.1.py
+++ Task-9.1.py
@@ -1,3 +1,12 @@
+def compare(S1,S2):
+    ngrams = [
+        S1[i:i+3] for i in range(len(S1))
+    ]
+    count = 0 
+    for ngram in ngrams:
+        count += S2.count(ngram)
+    return count/max(len(S1), len(S2))
+    
 from random import randint
 answers = ('да', 'нет', 'это сложный вопрос, я не знаю ответа')
 question = ""
@@ -5,6 +14,6 @@ while question != "стоп":
     print('Введите Ваш вопрос. Если Вы хотите завершить общение, то введите "стоп"')
     question = str(input())
     answer = answers[randint(0,len(answers)-1)]
-    if question == "стоп":
+    if compare('стоп', question) > 0.7:
         break
     print(answer)
