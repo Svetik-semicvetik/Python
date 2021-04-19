f92fdf15a36ad915131eb0efe120ae1a98c47382
Оськина Светлана Владимировна <oskina@ursalab.ru>
2021-04-19 23:19:46 +0500

    задание 2 к уроку 5

---
+++ Task-5.2.py
@@ -0,0 +1,10 @@
+def compare(S1,S2):
+    ngrams = [
+        S1[i:i+4] for i in range(len(S1))
+    ]
+    count = 0 
+    for ngram in ngrams:
+        count += S2.count(ngram)
+    return count/max(len(S1), len(S2))
+    
+compare('электроника','электричество')
